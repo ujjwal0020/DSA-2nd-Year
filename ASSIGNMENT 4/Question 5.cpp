ae940b1226bfe9b0cea38f772e0c89177541cdc5
ujjwal0020 <113961256+ujjwal0020@users.noreply.github.com>
2022-10-12 22:16:47 +0530

    Create Question 5.cpp

---
+++ ASSIGNMENT 4/Question 5.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    string s, t;
+    cin >> s >> t;
+    int k;
+    cin >> k;
+    
+    int i = 0, j = 0;
+    for (; i < (int)s.size() && j < (int)t.size(); ++i,++j) {
+        if (s[i] != t[j])
+            break;
+    }
+    
+    int p = ((int)s.size() - i) + ((int)t.size() - j);
+    if ((p <= k && (k-p) % 2 == 0) || k >= (int)s.size() + (int)t.size()) {
+        cout << "Yes";
+    } else {
+        cout << "No";}
+        return 0;
+        
+    }
